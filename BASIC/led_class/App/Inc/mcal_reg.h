76805b23d97c1d79f0f54001d4dafcf19d41d3df
slemanz <william_sleman@hotmail.com>
2025-07-30 03:58:12 -0300

    led class(feat): add main application

--- BASIC/led_class/App/Inc/mcal_reg.h
+++ BASIC/led_class/App/Inc/mcal_reg.h
@@ -1,1 +1,10 @@
 #include <cstdint>
+
+namespace mcal
+{
+    namespace reg
+    {
+        const std::uint32_t gpioa = 0x40020014;
+        const std::uint32_t gpio_pin5 = 5;
+    }
+}
--- BASIC/led_class/App/Src/main.cpp
+++ BASIC/led_class/App/Src/main.cpp
@@ -1,13 +1,14 @@
 /*
  * Blinky PA5
  */
-#include <stdint.h>
+#include "mcal_reg.h"
 
 // GPIOA = AHB1
 // GPIOA = 0x4002 0000
 // MODER_REG_OFFSET = 0x00
 // ODR_REG_OFFSET = 0x14
-// AHB1ENER = AHB1 + RCC + 0x30
+// AHB1ENR = AHB1 + RCC + 0x30
+#define RCC_AHB1ENR     (*((volatile uint32_t *)0x40023830U))
 
 class Led
 {
@@ -18,6 +19,24 @@ class Led
         Led(const port_type p, bval_type b):port(p),bval(b)
         {
             *reinterpret_cast<volatile uint32_t *>(port) &= ~(1 << bval);
+
+            /* set pin mode to output */
+            const port_type gpio_mode_reg = port - 0x14;
+            *reinterpret_cast<volatile port_type *>(gpio_mode_reg) |= (1U << (bval*2));
+        }
+
+        void toggle()const
+        {
+            *reinterpret_cast<volatile port_type *>(port) ^= (1U << bval);
+        }
+
+        void pseudo_delayMs(int n)
+        {
+            int i;
+            for(; n > 0; n--){
+                for(i=0; i < 3200; i++);
+            }
+
         }
     
     private:
@@ -28,4 +47,12 @@ class Led
 
 int main(void)
 {
+    RCC_AHB1ENR |= (1U << 0); // enable clock to GPIOA
+    Led led5(mcal::reg::gpioa, mcal::reg::gpio_pin5);
+
+    while(1)
+    {
+        led5.toggle();
+        led5.pseudo_delayMs(500);
+    }
 }
