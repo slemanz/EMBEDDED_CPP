d903549518e63978271c5ffef0a3475246f1b1d1
slemanz <william_sleman@hotmail.com>
2025-07-30 05:33:12 -0300

    essential(feat): add things to linker

--- BASIC/essential/Linkers/startup.c
+++ BASIC/essential/Linkers/startup.c
@@ -4,7 +4,7 @@
  * STM32F401xE
  */
 #define SRAM_START      0x20000000U
-#define SRAM_SIZE       (60 * 1024)
+#define SRAM_SIZE       (128 * 1024)
 #define SRAM_END        ((SRAM_START) + (SRAM_SIZE))
 #define STACK_START     (SRAM_END)
 
