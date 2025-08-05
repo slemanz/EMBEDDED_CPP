b7974caf1547e5ca460fea3336e18571eaeefb85
slemanz <william_sleman@hotmail.com>
2025-08-05 05:43:18 -0300

    gpio(feat): begin to implement cpp

--- BASIC/gpio/Drivers/Src/driver_gpio.cpp
+++ BASIC/gpio/Drivers/Src/driver_gpio.cpp
@@ -15,3 +15,15 @@ void Pin::configure(Mode mode, OutputType otype, Speed speed, Pull pull, uint8_t
         set_alternate_function(alternate);
     }
 }
+
+void Pin::set_mode(Mode mode) const
+{
+    constexpr uint32_t moder_offset = 0x00;
+    const uint32_t moder_addr = static_cast<uint32_t>(port_) + moder_offset;
+    const uint32_t shift = 2 * pin_;
+
+    mcal::reg::reg_access<uint32_t, uint32_t, moder_addr, ~(0x3U << shift)>::reg_and();
+
+    MCAL_REG_ACCESS(uint32_t, uint32_t, moder_addr)
+        ::reg_or(static_cast<uint32_t>(mode) << shift);
+}
