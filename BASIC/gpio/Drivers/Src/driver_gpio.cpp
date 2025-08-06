4194d66663bf603d9b392f814d7ad66176dd63ef
slemanz <william_sleman@hotmail.com>
2025-08-06 04:03:45 -0300

    gpio(feat): implement set output mode and output type

--- BASIC/gpio/Drivers/Src/driver_gpio.cpp
+++ BASIC/gpio/Drivers/Src/driver_gpio.cpp
@@ -1,29 +1,47 @@
 #include "driver_gpio.hpp"
-#include "mcal_reg_access.hpp"
+#include "mcal_reg.hpp"
 
 using namespace driver::gpio;
+using namespace mcal::reg;
 
 void Pin::configure(Mode mode, OutputType otype, Speed speed, Pull pull, uint8_t alternate) const
 {
+    enable_clock();
+
     set_mode(mode);
     set_output_type(otype);
-    set_speed(speed);
-    set_pull(pull);
+    //set_speed(speed);
+    //set_pull(pull);
 
     if(mode == Mode::Alternate)
     {
-        set_alternate_function(alternate);
+        //set_alternate_function(alternate);
     }
 }
 
 void Pin::set_mode(Mode mode) const
 {
-    constexpr uint32_t moder_offset = 0x00;
-    const uint32_t moder_addr = static_cast<uint32_t>(port_) + moder_offset;
+    const uint32_t moder_addr = get_port_base() + gpio_offset::moder;
     const uint32_t shift = 2 * pin_;
 
-    mcal::reg::reg_access<uint32_t, uint32_t, moder_addr, ~(0x3U << shift)>::reg_and();
+    reg_access::reg_not(moder_addr, (0x3 << shift));
+    reg_access::reg_or(moder_addr, (static_cast<uint32_t>(mode) << shift));
+}
 
-    MCAL_REG_ACCESS(uint32_t, uint32_t, moder_addr)
-        ::reg_or(static_cast<uint32_t>(mode) << shift);
+void Pin::set_output_type(OutputType otype) const
+{
+    const uint32_t otyper_addr = get_port_base() + gpio_offset::otyper;
+    reg_access::reg_not(otyper_addr, (0x1 << pin_));
+    reg_access::reg_or(otyper_addr, (static_cast<uint32_t>(otype) << pin_));
+}
+
+void Pin::toggle() const
+{
+    const uint32_t odr_addr = (get_port_base()) + gpio_offset::odr;
+    reg_access::bit_not(odr_addr, pin_);
+}
+
+void Pin::enable_clock() const
+{
+    if(port_ == Port::A) reg_access::reg_or(ahb1enr, (1 << 0));
 }
