4763247a827879b22d4706a85c2bc9f362f837b6
slemanz <william_sleman@hotmail.com>
2025-08-07 04:43:24 -0300

    uart(feat): improve usart mcal

--- BASIC/uart/Drivers/Inc/mcal_reg.hpp
+++ BASIC/uart/Drivers/Inc/mcal_reg.hpp
@@ -8,6 +8,11 @@ namespace mcal
 {
     namespace reg
     {
+        constexpr uint32_t BIT(uint32_t pos)
+        {
+            return (1U << pos);
+        }
+
         // global base address
         constexpr std::uint32_t periph_base = 0x40000000U;
 
@@ -49,19 +54,92 @@ namespace mcal
             constexpr std::uint32_t afrh        = 0x24;  
         } // gpio_offset
 
-        // usart register offset
-        namespace usart_offset
-        {   
-            constexpr std::uint32_t sr          = 0x00;
-            constexpr std::uint32_t dr          = 0x04;
-            constexpr std::uint32_t brr         = 0x08;
-            constexpr std::uint32_t cr1         = 0x0C;
-            constexpr std::uint32_t cr2         = 0x10;
-            constexpr std::uint32_t cr3         = 0x14;
-            constexpr std::uint32_t gtpr        = 0x18;
-        } // usart_offset
+
+        namespace usart
+        {
+            // USART register offsets
+            namespace offset
+            {
+                constexpr std::uint32_t sr         = 0x00;
+                constexpr std::uint32_t dr         = 0x04;
+                constexpr std::uint32_t brr        = 0x08;
+                constexpr std::uint32_t cr1        = 0x0c;
+                constexpr std::uint32_t cr2        = 0x10;
+                constexpr std::uint32_t cr3        = 0x14;
+                constexpr std::uint32_t gtpr       = 0x18;
+            }
+
+            /// Bit positions (for shift operations and mask generation)
+            namespace bitpos
+            {
+                namespace cr1
+                {
+                    constexpr uint32_t sbk          = 0;    ///< Send break
+                    constexpr uint32_t rwu          = 1;    ///< Receiver wakeup
+                    constexpr uint32_t re           = 2;    ///< Receiver enable
+                    constexpr uint32_t te           = 3;    ///< Transmitter enable
+                    constexpr uint32_t idleie       = 4;    ///< IDLE interrupt enable
+                    constexpr uint32_t rxneie       = 5;    ///< RX not empty interrupt enable
+                    constexpr uint32_t tcie         = 6;    ///< Transmission complete interrupt enable
+                    constexpr uint32_t txeie        = 7;    ///< TX empty interrupt enable
+                    constexpr uint32_t peie         = 8;    ///< Parity error interrupt enable
+                    constexpr uint32_t ps           = 9;    ///< Parity selection
+                    constexpr uint32_t pce          = 10;   ///< Parity control enable
+                    constexpr uint32_t wake         = 11;   ///< Wakeup method
+                    constexpr uint32_t m            = 12;   ///< Word length
+                    constexpr uint32_t ue           = 13;   ///< USART enable
+                } // namespace cr1
+
+                namespace sr
+                {
+                    constexpr uint32_t pe           = 0;
+                    constexpr uint32_t fe           = 1;
+                    constexpr uint32_t nf           = 2;
+                    constexpr uint32_t ore          = 3;
+                    constexpr uint32_t idle         = 4;
+                    constexpr uint32_t rxne         = 5;
+                    constexpr uint32_t tc           = 6;
+                    constexpr uint32_t txe          = 7;
+                    constexpr uint32_t busy         = 8;
+                } // namespace sr1
+            } //namespace bitpos
+            
+            namespace mask
+            {
+                namespace cr1
+                {
+                    constexpr uint32_t sbk      = BIT(bitpos::cr1::sbk);
+                    constexpr uint32_t rwu      = BIT(bitpos::cr1::rwu);
+                    constexpr uint32_t re       = BIT(bitpos::cr1::re);
+                    constexpr uint32_t te       = BIT(bitpos::cr1::te);
+                    constexpr uint32_t idleie   = BIT(bitpos::cr1::idleie);
+                    constexpr uint32_t rxneie   = BIT(bitpos::cr1::rxneie);
+                    constexpr uint32_t tcie     = BIT(bitpos::cr1::tcie);
+                    constexpr uint32_t txeie    = BIT(bitpos::cr1::txeie);
+                    constexpr uint32_t peie     = BIT(bitpos::cr1::peie);
+                    constexpr uint32_t ps       = BIT(bitpos::cr1::ps);
+                    constexpr uint32_t pce      = BIT(bitpos::cr1::pce);
+                    constexpr uint32_t wake     = BIT(bitpos::cr1::wake);
+                    constexpr uint32_t m        = BIT(bitpos::cr1::m);
+                    constexpr uint32_t ue       = BIT(bitpos::cr1::ue);
+                } // namespace cr1
+
+                namespace sr
+                {
+                    constexpr uint32_t pe       = BIT(bitpos::sr::pe);
+                    constexpr uint32_t fe       = BIT(bitpos::sr::fe);
+                    constexpr uint32_t nf       = BIT(bitpos::sr::nf);
+                    constexpr uint32_t ore      = BIT(bitpos::sr::ore);
+                    constexpr uint32_t idle     = BIT(bitpos::sr::idle);
+                    constexpr uint32_t rxne     = BIT(bitpos::sr::rxne);
+                    constexpr uint32_t tc       = BIT(bitpos::sr::tc);
+                    constexpr uint32_t txe      = BIT(bitpos::sr::txe);
+                    constexpr uint32_t busy     = BIT(bitpos::sr::busy);
+                } // namespace sr
+            } // namespace mask
+        } // namespace uart
+
     } // reg
 } // mcal
-
 
 #endif /* INC_MCAL_REG_HPP */
