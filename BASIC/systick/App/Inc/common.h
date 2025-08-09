6f2420b81980f5b5dd665f9dc3ae85aae8a31af1
slemanz <william_sleman@hotmail.com>
2025-08-09 09:02:40 -0300

    systick(feat): add config file

--- BASIC/systick/App/Inc/common.h
+++
@@ -1,3 +0,0 @@
-#ifndef INC_COMMON_H_
-#define INC_COMMON_H_
-#endif /* INC_COMMON_H_ */
--- BASIC/systick/App/Src/main.cpp
+++ BASIC/systick/App/Src/main.cpp
@@ -1,47 +1,14 @@
-#include "driver_gpio.hpp"
-#include "driver_uart.hpp"
-#include "driver_systick.hpp"
-#include <cstring>
+#include "config.hpp"
 #include <cstdio>
-
-using namespace driver;
-
-uart::Uart serial(uart::Instance::USART2);
-
-extern "C" int __io_putchar(int ch)
-{
-    serial.write(reinterpret_cast<uint8_t*>(&ch), 1);
-    return ch;
-}
-
-extern "C" void SysTick_Handler(void)
-{
-    systick::Systick::ticks_increment();
-}
 
 int main(void)
 {
-    gpio::Pin pa5(gpio::Port::A, 5);
-    pa5.configure(gpio::Mode::Output);
-
-    gpio::Pin pa2(gpio::Port::A, 2);
-    pa2.configure(  gpio::Mode::Alternate,
-                    gpio::OutputType::PushPull,
-                    gpio::Speed::High,
-                    gpio::Pull::None,
-                    mcal::reg::gpio::altfn::pa2_usart2_tx);
-
-    serial.init(uart::BaudRate::BR_9600, uart::Mode::Tx);
-    systick::Systick::init();
-
-    for(uint32_t i = 0; i < 500000; i++); // delay to stable pins
-
-    printf("Board init!!\n");
+    config_drivers();
+    printf("\nBoard init!!\n");
     
     while(1)
     {
         pa5.toggle();
-        systick::Systick::delay_ms(500);
+        driver::systick::Systick::delay_ms(200);
     }
 }
-
